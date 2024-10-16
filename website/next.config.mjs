beb150a0f16268c8abd82a6e2797881b3e4f8048
Ricardo de Sousa <ricardo.desousa96@gmail.com>
2024-10-16 12:18:57 +0200

    Add changes

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     output: 'export',
+    
+    // Set the basePath and assetPrefix for GitHub Pages (or similar platforms)
+    basePath: isProd ? '/blue-echo-gym-website' : '',
+    assetPrefix: isProd ? '/blue-echo-gym-website/' : '',
 
     images: {
-        unoptimized: true, // If using images, especially with static exports
-      },
+        unoptimized: true, // Disable image optimization for static export
+    },
 };
 
 export default nextConfig;
